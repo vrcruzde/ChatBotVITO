253a1f23eaab80837f69d919bf8f912164398f0e
vrcruzde <virocde@hotmail.com>
2023-12-20 23:29:58 -0500

    Inicio proyecto ChatBotVITO

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+//const apiRoute = require("./routes/routes");
+
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send("Hola mundo mundo")
+})
+
+const PORT = process.env.PORT || 3000;
+
+//app.use(express.json());
+
+//app.use("/whatsapp", apiRoute);
+
+app.listen(PORT, () => {console.log("el puerto es: " + PORT)});
