1157ee6afe8155eb3bedbf37859ff94b0eecfaa1
vrcruzde <virocde@hotmail.com>
2023-12-21 00:28:03 -0500

    Url con /whatsapp

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/", apiRoute);
+app.use("/whatsapp", apiRoute);
 
 app.listen(PORT, () => {console.log("el puerto es: " + PORT)});
