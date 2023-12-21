a6691d282893c1d8e6312e58afa91b673bc7fbd1
vrcruzde <virocde@hotmail.com>
2023-12-21 00:23:56 -0500

    Creando VerifyToken

--- src/controllers/whatsappControllers.js
+++ src/controllers/whatsappControllers.js
@@ -1,5 +1,18 @@
 const VerifyToken = (req, res) => {
-   res.send("hola VeryfyToken")
+    try{
+        var accessToken = "RTQWWTVHBDEJHJKIKIKNDS9090DS";
+        var token = req.query["hub.verify_token"];
+        var challenge = req.query["hub.challenge"];
+
+        if(challenge != null && token != null && token == accessToken){
+            res.send(challenge);
+        }else{
+            res.status(400).send();
+        }
+
+    }catch(e){
+        res.status(400).send();
+    }
 }
 
 const ReceivedMessage = (req, res) => {
